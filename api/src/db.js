9319b3935df2737ed49c17e13735ace92ef8c192
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-12-09 11:49:43 -0500

    Server anD DB

--- api/src/db.js
+++ api/src/db.js
@@ -21,7 +21,7 @@ fs.readdirSync(path.join(__dirname, '/models'))
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+// Injectamos la conexion (sequelize) a todos los modelos  --> Se inicializan?
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Breed } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Dog.belongsToMany(Breed, { through: 'Dog_breed'});
+Breed.belongsToMany(Dog, { through: 'Dog_breed'} )
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,31 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    image: {
+      type: DataTypes.JSON, // Imagen como objeto JSON
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+    height: {
+      type: DataTypes.JSON, // Altura como objeto JSON (cm)
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.JSON, // Peso como objeto JSON (kg)
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.STRING, // Años de vida como string
+      allowNull: false
+    }
   });
 };
