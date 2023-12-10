32c351837efbb8527ac6c26a161ded312ae2cf7a
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-12-10 13:17:24 -0500

    Request without storing in DB

--- api/src/controllers/Breeds/getAllDogs.js
+++ api/src/controllers/Breeds/getAllDogs.js
@@ -6,49 +6,28 @@ const { API_KEY } = process.env;
 
 async function getAllDogs(req, res){
     try {
-        let allDogs = await Breeds.findAll({
+        // Fetch dogs from the local database
+        const localDogs = await Breeds.findAll({
             include: Temperaments
         });
 
-        // If there are less than 24 dogs in our database, fill in with API data
-        if (allDogs.length < 24) {
-            const apiUrl = 'https://api.thedogapi.com/v1/breeds';
-            const apiResponse = await axios.get(apiUrl, {
-                headers: { 'x-api-key': API_KEY }
-            });
-            const apiDogs = apiResponse.data.slice(0, 24 - allDogs.length);
+        // Fetch dogs from the API
+        const apiUrl = 'https://api.thedogapi.com/v1/breeds';
+        const apiResponse = await axios.get(apiUrl, {
+            headers: { 'x-api-key': API_KEY }
+        });
+        const apiDogs = apiResponse.data.map(dog => ({
+            id: dog.id,
+            name: dog.name,
+            height: dog.height.metric,
+            weight: dog.weight.metric,
+            life_span: dog.life_span,
+            image: dog.image?.url,
+            temperaments: dog.temperament ? dog.temperament.split(', ').map(temp => temp.trim()) : []
+        }));
 
-            for (const dog of apiDogs) {
-                let [breed, created] = await Breeds.findOrCreate({
-                    where: { name: dog.name },
-                    defaults: {
-                        id: dog.id,
-                        image: dog.image?.url,
-                        name: dog.name,
-                        height: dog.height.metric,
-                        weight: dog.weight.metric,
-                        life_span: dog.life_span
-                    }
-                });
-
-                if (created && dog.temperament) {
-                    let temperaments = dog.temperament.split(', ').map(t => t.trim());
-                    for (const tempName of temperaments) {
-                        let [temp, tempCreated] = await Temperaments.findOrCreate({
-                            where: { name: tempName }
-                        });
-                        if (tempCreated) {
-                            await breed.addTemperament(temp);
-                        }
-                    }
-                }
-            }
-
-            // Refetch all dogs after insertion
-            allDogs = await Breeds.findAll({
-                include: Temperaments
-            });
-        }
+        // Combine local and API dogs
+        const allDogs = [...localDogs, ...apiDogs];
 
         res.status(200).json(allDogs);
     } catch (error) {
