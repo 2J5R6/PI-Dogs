5a3f2ce9609571f03f1a44517c96bc3940bbe032
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-12-30 19:01:05 -0500

    WORKING HOMEPAGE

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,15 +7,15 @@ const Card = ({ dog }) => {
     <div className={styles.cardContainer}>
       <div className={styles.cardInner}>
         <div className={styles.cardFront}>
-          <img src={dog.image} alt={dog.name} className={styles.dogImage} />
+          <img src={dog.image.url || dog.image} alt={dog.name} className={styles.dogImage} />
           <h2 className={styles.dogName}>{dog.name.toUpperCase()}</h2>
           <div className={styles.temperaments}>
             {dog.temperaments && dog.temperaments.map((temp, index) => (
-              <span key={index} className={styles.temperament}>{typeof temp === 'string' ? temp.toUpperCase() : ''}</span>
+              <span key={index} className={styles.temperament}>{typeof temp === 'string' ? temp.toUpperCase() : temp.name.toUpperCase()}</span>
             ))}
           </div>
           <div className={styles.weightRange}>
-            <span className={styles.weight}>WEIGHT: {dog.weight}</span>
+            <span className={styles.weight}>WEIGHT: {dog.weight.metric || dog.weight}</span>
           </div>
         </div>
         <div className={styles.cardBack}>
@@ -24,10 +24,10 @@ const Card = ({ dog }) => {
             <p>TEMPERAMENTS:</p>
             <ul>
               {dog.temperaments && dog.temperaments.map((temp, index) => (
-                <li key={index} className={styles.temperament}>{typeof temp === 'string' ? temp.toUpperCase() : ''}</li>
+                <li key={index} className={styles.temperament}>{typeof temp === 'string' ? temp.toUpperCase() : temp.name.toUpperCase()}</li>
               ))}
             </ul>
-            <p>WEIGHT: {dog.weight}</p>
+            <p>WEIGHT: {dog.weight.metric || dog.weight}</p>
           </div>
           <Link to={`/dog/${dog.id}`} className={styles.detailsButton}>DETAILS</Link>
         </div>
