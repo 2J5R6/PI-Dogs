d9d5cee70783be008b12054e44db2ff92bfccb3e
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-12-31 11:44:09 -0500

    Working on forms

--- client/src/views/FormPage/FormPage.jsx
+++ client/src/views/FormPage/FormPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { createDog } from '../../redux/actions/actions'; // Asegúrate de que esta acción esté definida en tus actions de Redux
+import { createDog } from '../../redux/actions/actions';
 import styles from './FormPage.module.css';
 
 const FormPage = () => {
-  const [formData, setFormData] = useState({
+  const dispatch = useDispatch();
+  const initialFormState = {
     name: '',
     heightMin: '',
     heightMax: '',
@@ -13,17 +14,42 @@ const FormPage = () => {
     life_span: '',
     image: '',
     temperaments: [],
-  });
+  };
+  const [formData, setFormData] = useState(initialFormState);
+  const [formErrors, setFormErrors] = useState({});
   const [tempInput, setTempInput] = useState('');
-  const dispatch = useDispatch();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    validateField(name, value);
   };
 
-  const handleTempInputChange = (e) => {
-    setTempInput(e.target.value);
+  const validateField = (name, value) => {
+    let errors = { ...formErrors };
+    switch (name) {
+      case 'name':
+        errors.name = value.trim() ? '' : 'Name is required.';
+        break;
+      case 'heightMin':
+      case 'heightMax':
+        const minHeight = formData.heightMin || value;
+        const maxHeight = formData.heightMax || value;
+        errors.height = (minHeight < maxHeight) ? '' : 'Invalid height range.';
+        break;
+      case 'weightMin':
+      case 'weightMax':
+        const minWeight = formData.weightMin || value;
+        const maxWeight = formData.weightMax || value;
+        errors.weight = (minWeight < maxWeight) ? '' : 'Invalid weight range.';
+        break;
+      case 'life_span':
+        errors.life_span = value ? '' : 'Life span is required.';
+        break;
+      default:
+        break;
+    }
+    setFormErrors(errors);
   };
 
   const handleAddTemperament = () => {
@@ -33,43 +59,40 @@ const FormPage = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Validate form data before dispatching
-    if (!formData.name || /\d/.test(formData.name)) {
-      return alert('Please enter a valid breed name without numbers.');
+    if (Object.values(formErrors).some(error => error)) {
+      alert('Please correct the errors in the form.');
+      return;
     }
-    if (parseInt(formData.heightMin) >= parseInt(formData.heightMax)) {
-      return alert('Minimum height should be less than maximum height.');
-    }
-    if (parseInt(formData.weightMin) >= parseInt(formData.weightMax)) {
-      return alert('Minimum weight should be less than maximum weight.');
-    }
-    // Construct the dog object
-    const newDog = {
+    const dogData = {
       name: formData.name,
-      height: { metric: `${formData.heightMin} - ${formData.heightMax}` },
-      weight: { metric: `${formData.weightMin} - ${formData.weightMax}` },
-      life_span: formData.life_span,
-      image: { url: formData.image },
-      temperaments: formData.temperaments,
+      height: `${formData.heightMin} - ${formData.heightMax}`,
+      weight: `${formData.weightMin} - ${formData.weightMax}`,
+      life_span: `${formData.life_span} years`,
+      image: formData.image,
+      temperaments: formData.temperaments.join(", "),
     };
-    dispatch(createDog(newDog));
+    try {
+      const response = await dispatch(createDog(dogData));
+      if (response.type.includes('SUCCESS')) {
+        alert("Dog created successfully!");
+        setFormData(initialFormState);
+      } else {
+        alert("Failed to create dog. Please try again.");
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again.");
+    }
   };
 
   return (
     <div className={styles.formContainer}>
-      <div className={styles.paw}></div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-
+        {/* Name field */}
+        {/* ... */}
+        
+        {/* Height fields */}
         <label htmlFor="heightMin">Minimum Height</label>
         <input
           type="number"
@@ -78,6 +101,7 @@ const FormPage = () => {
           value={formData.heightMin}
           onChange={handleInputChange}
         />
+        {formErrors.height && <div className={styles.error}>{formErrors.height}</div>}
 
         <label htmlFor="heightMax">Maximum Height</label>
         <input
@@ -88,57 +112,68 @@ const FormPage = () => {
           onChange={handleInputChange}
         />
 
-        <label htmlFor="weightMin">Minimum Weight</label>
-        <input
-          type="number"
-          id="weightMin"
-          name="weightMin"
-          value={formData.weightMin}
-          onChange={handleInputChange}
-        />
+        {/* Weight fields */}
+<label htmlFor="weightMin">Minimum Weight</label>
+<input
+  type="number"
+  id="weightMin"
+  name="weightMin"
+  value={formData.weightMin}
+  onChange={handleInputChange}
+/>
+{formErrors.weight && <div className={styles.error}>{formErrors.weight}</div>}
 
-        <label htmlFor="weightMax">Maximum Weight</label>
-        <input
-          type="number"
-          id="weightMax"
-          name="weightMax"
-          value={formData.weightMax}
-          onChange={handleInputChange}
-        />
+<label htmlFor="weightMax">Maximum Weight</label>
+<input
+  type="number"
+  id="weightMax"
+  name="weightMax"
+  value={formData.weightMax}
+  onChange={handleInputChange}
+/>
 
-        <label htmlFor="life_span">Life Span</label>
-        <input
-          type="text"
-          id="life_span"
-          name="life_span"
-          value={formData.life_span}
-          onChange={handleInputChange}
-        />
+{/* Life span field */}
+<label htmlFor="life_span">Life Span (years)</label>
+<input
+  type="number"
+  id="life_span"
+  name="life_span"
+  value={formData.life_span}
+  onChange={handleInputChange}
+/>
+{formErrors.life_span && <div className={styles.error}>{formErrors.life_span}</div>}
 
-        <div className={styles.temperamentSection}>
-          <label htmlFor="temperament">Temperament</label>
-          <input
-            type="text"
-            id="temperament"
-            value={tempInput}
-            onChange={handleTempInputChange}
-          />
-          <button type="button" onClick={handleAddTemperament}>
-            Add Temperament
-          </button>
-        </div>
+{/* Image URL field */}
+<label htmlFor="image">Image URL</label>
+<input
+  type="text"
+  id="image"
+  name="image"
+  value={formData.image}
+  onChange={handleInputChange}
+/>
+{formData.image && (
+  <img src={formData.image} alt="Dog preview" className={styles.imagePreview} />
+)}
 
-        <div className={styles.addedTemperaments}>
-          {formData.temperaments.map((temp, index) => (
-            <div key={index} className={styles.temperamentTag}>
-              {temp}
-            </div>
-          ))}
-        </div>
+{/* Temperaments field */}
+<label htmlFor="temperament">Temperament</label>
+<input
+  type="text"
+  id="temperament"
+  name="temperament"
+  value={tempInput}
+  onChange={(e) => setTempInput(e.target.value)}
+/>
+<button type="button" onClick={handleAddTemperament}>Add Temperament</button>
+<div className={styles.temperamentTags}>
+  {formData.temperaments.map((temp, index) => (
+    <span key={index} className={styles.temperamentTag}>{temp}</span>
+  ))}
+</div>
 
-        <button type="submit" className={styles.createButton}>
-          Create Dog
-        </button>
+        <button type="submit">Create Dog</button>
+        <button type="button" onClick={() => setFormData(initialFormState)}>Cancel</button>
       </form>
     </div>
   );
