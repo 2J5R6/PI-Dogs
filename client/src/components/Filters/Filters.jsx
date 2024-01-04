2a631e5006f545176b480b2094e742fca26993da
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2024-01-04 12:55:22 -0500

    Creting filter temmplate

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -1,94 +1,88 @@
-// // Filters.js
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   setDataSource,
-//   sortByIdAsc,
-//   sortByIdDesc,
-//   sortByNameAsc,
-//   sortByNameDesc,
-//   filterPokemonsByType
-// } from '../../redux/actions/pokemonActions';
-// import styles from './Filters.module.css';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  filterByTemperament,
+  filterByOrigin,
+  sortByWeight,
+  sortByName,
+  filterByLifeSpan
+} from '../redux/actions';
+import styles from './Filters.module.css'; // Asegúrate de tener un archivo Filters.module.css
 
-// const Filters = () => {
-//   const dispatch = useDispatch();
-//   const dataSource = useSelector(state => state.pokemons.dataSource);
-//   // const searchedPokemons = useSelector(state => state.pokemons.searchedPokemons);
+const Filters = () => {
+  const dispatch = useDispatch();
+  const temperaments = useSelector((state) => state.temperaments);
+  const [selectedTemperament, setSelectedTemperament] = useState('');
+  const [selectedOrigin, setSelectedOrigin] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+  const [lifeSpanRange, setLifeSpanRange] = useState('');
 
-//   const handleDataSourceChange = (e) => {
-//     dispatch(setDataSource(e.target.value));
-//   };
+  const handleTemperamentChange = (e) => {
+    setSelectedTemperament(e.target.value);
+    dispatch(filterByTemperament(e.target.value));
+  };
 
-//   const handleSortChange = (e) => {
-//     switch (e.target.value) {
-//       case 'id-asc':
-//         dispatch(sortByIdAsc());
-//         break;
-//       case 'id-desc':
-//         dispatch(sortByIdDesc());
-//         break;
-//       case 'name-asc':
-//         dispatch(sortByNameAsc());
-//         break;
-//       case 'name-desc':
-//         dispatch(sortByNameDesc());
-//         break;
-//       default:
-//         break;
-//     }
-//   };
+  const handleOriginChange = (e) => {
+    setSelectedOrigin(e.target.value);
+    dispatch(filterByOrigin(e.target.value));
+  };
 
-//   const handleTypeFilter = (e) => {
-//     const selectedType = e.target.value;
-//     dispatch(filterPokemonsByType(selectedType));
-// };
+  const handleSortChange = (e) => {
+    setSortOrder(e.target.value);
+    e.target.value === 'name' ? dispatch(sortByName()) : dispatch(sortByWeight());
+  };
 
+  const handleLifeSpanChange = (e) => {
+    setLifeSpanRange(e.target.value);
+    dispatch(filterByLifeSpan(e.target.value));
+  };
 
-//   return (
-//     <div className={styles.filtersContainer}>
-//       <div className={styles.dataSourceContainer}>
-//         <label>Data Source:</label>
-//         <select value={dataSource} onChange={handleDataSourceChange}>
-//           <option value="db">Database</option>
-//           <option value="api">API</option>
-//         </select>
-//       </div>
-//       <div className={styles.sortContainer}>
-//         <label>Sort By:</label>
-//         <select onChange={handleSortChange}>
-//           <option value="id-asc">ID Ascending</option>
-//           <option value="id-desc">ID Descending</option>
-//           <option value="name-asc">Name Ascending</option>
-//           <option value="name-desc">Name Descending</option>
-//         </select>
-//       </div>
-//       <div className={styles.typeFilterContainer}>
-//         <label>Filter by Type:</label>
-//         <select onChange={handleTypeFilter}>
-//           <option value="">All</option>
-//           <option value="normal">Normal</option>
-//           <option value="fire">Fire</option>
-//           <option value="water">Water</option>
-//           <option value="electric">Electric</option>
-//           <option value="grass">Grass</option>
-//           <option value="ice">Ice</option>
-//           <option value="fighting">Fighting</option>
-//           <option value="poison">Poison</option>
-//           <option value="ground">Ground</option>
-//           <option value="flying">Flying</option>
-//           <option value="psychic">Psychic</option>
-//           <option value="bug">Bug</option>
-//           <option value="rock">Rock</option>
-//           <option value="ghost">Ghost</option>
-//           <option value="dark">Dark</option>
-//           <option value="dragon">Dragon</option>
-//           <option value="steel">Steel</option>
-//           <option value="fairy">Fairy</option>
-//         </select>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className={styles.filtersContainer}>
+      {/* Temperament Filter */}
+      <div className={styles.filterItem}>
+        <label>Temperament:</label>
+        <select value={selectedTemperament} onChange={handleTemperamentChange} className={styles.filterSelect}>
+          <option value="">All Temperaments</option>
+          {temperaments.map((temp) => (
+            <option key={temp} value={temp}>{temp}</option>
+          ))}
+        </select>
+      </div>
 
-// export default Filters;
+      {/* Origin Filter */}
+      <div className={styles.filterItem}>
+        <label>Origin:</label>
+        <select value={selectedOrigin} onChange={handleOriginChange} className={styles.filterSelect}>
+          <option value="">All Origins</option>
+          <option value="api">API</option>
+          <option value="db">Database</option>
+        </select>
+      </div>
+
+      {/* Life Span Filter */}
+      <div className={styles.filterItem}>
+        <label>Life Span:</label>
+        <input
+          type="text"
+          value={lifeSpanRange}
+          onChange={handleLifeSpanChange}
+          placeholder="e.g., 10-12"
+          className={styles.filterInput}
+        />
+      </div>
+
+      {/* Sorting Options */}
+      <div className={styles.filterItem}>
+        <label>Sort By:</label>
+        <select value={sortOrder} onChange={handleSortChange} className={styles.filterSelect}>
+          <option value="">Default</option>
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
